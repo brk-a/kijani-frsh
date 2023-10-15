64534dc669ea1269026de68075c23259a9086e45
brk-a <fnjakai@outlook.com>
2023-10-15 07:15:14 +0300

    down load functionality, part 1

---
+++ backoffice/frsh-back-office/pages/api/download-pur-order.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+const downloadPurchaseOrder = async (req, res) => {
+    if (req.method === "GET") {
+        try {
+            const { filename } = req.query
+            const DUMMY_URL = ""
+            const { data } = await axios.get(DUMMY_URL, {
+                responseType: "stream",
+            })
+            res.setHeader("content-disposition", `attachment; filename=${filename}`)
+            data.pipe(res)
+        } catch (error) {
+            console.info("downloadPurchaseOrder: ", error)
+            res.status(400).json({message: "downloadPurchaseOrder: Bad request"})
+        }
+    } else {
+        res.status(405).json({ message: "downloadPurchaseOrder: Not allowed" })
+    }
+}
